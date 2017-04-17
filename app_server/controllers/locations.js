b3c104099c36ba4c3b6995ed5599557d731da6a4
Eswara <eswaraprasadh@yahoo.com>
2017-04-17 05:15:43 +0000

    location related pages

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,15 +1,16 @@
 /**
  * Created by eswaraprasadh on 16-04-2017.
  */
+/*eslint-env node, node, node*/
 module.exports.homelist = function (req,res) {
-  res.render('index',{title:'Home'});
+  res.render('locations-list',{title:'Home'});
 };
 
 module.exports.locationInfo = function (req,res) {
-    res.render('index',{title:'Location Info'});
+    res.render('location-info',{title:'Location Info'});
 };
 
 
 module.exports.addReview = function (req,res) {
-    res.render('index',{title:'Add Review'});
+    res.render('location-review-form',{title:'Add Review'});
 };
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -2,5 +2,5 @@
  * Created by eswaraprasadh on 16-04-2017.
  */
 module.exports.about= function (req,res) {
-    res.render('index',{title:'About'})
+    res.render('generic-text',{title:'About'})
 };
